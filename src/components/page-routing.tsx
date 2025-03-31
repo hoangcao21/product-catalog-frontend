cef338d125575e017214342a5e67dd686d3c081b
hoangcao21 <hoangcao21@github.com>
2025-03-31 07:45:39 +0700

    chore: reuse page paths

--- src/components/page-routing.tsx
+++ src/components/page-routing.tsx
@@ -2,9 +2,14 @@ import { AuthPage } from '../pages/auth';
 import { HomePage } from '../pages/home';
 import { ProductDetailPage } from '../pages/product-detail';
 import { SplashPage } from '../pages/splash';
+import {
+  PATH_PAGE_AUTH,
+  PATH_PAGE_HOME,
+  PATH_PAGE_PRODUCT_DETAIL,
+} from '../shared/routes';
 import { EntryPoint } from './entry-point';
 import { FC } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 export const PageRouting: FC = () => {
   return (
@@ -12,10 +17,10 @@ export const PageRouting: FC = () => {
       <Routes>
         <Route element={<EntryPoint />}>
           <Route index element={<SplashPage />} />
-          <Route path="auth" element={<AuthPage />} />
-          <Route path="home" element={<HomePage />} />
+          <Route path={PATH_PAGE_AUTH} element={<AuthPage />} />
+          <Route path={PATH_PAGE_HOME} element={<HomePage />} />
           <Route
-            path="product-detail/:productId"
+            path={PATH_PAGE_PRODUCT_DETAIL}
             element={<ProductDetailPage />}
           />
         </Route>
