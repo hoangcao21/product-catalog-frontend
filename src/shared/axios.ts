cf06f1bd57b1e574e8ae08cbd9b80ce7ec5da2c2
hoangcao21 <hoangcao21@github.com>
2025-03-31 07:46:53 +0700

    chore: custom axios instance

---
+++ src/shared/axios.ts
@@ -0,0 +1,78 @@
+import { BACKEND_URL } from './config';
+import { authUtils } from './cookie';
+import Axios, {
+  AxiosInstance,
+  HttpStatusCode,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import * as qs from 'qs';
+
+export const axiosInstance: AxiosInstance = Axios.create({
+  baseURL: BACKEND_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use((requestConfig) => {
+  const newRequestConfig: InternalAxiosRequestConfig = {
+    ...requestConfig,
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ...(requestConfig.headers as any),
+
+      Authorization: authUtils.getToken()
+        ? `Bearer ${authUtils.getToken()}`
+        : undefined,
+    },
+    paramsSerializer(params) {
+      return qs.stringify(params);
+    },
+  };
+
+  return newRequestConfig;
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response, // Directly return successful responses.
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === HttpStatusCode.Unauthorized &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true; // Mark the request as retried to avoid infinite loops.
+
+      try {
+        // const refreshToken = authUtils.getRefreshToken();
+
+        // const response = {}; // TODO: Invoke the refresh API from "api.ts"
+
+        // const { accessToken, refreshToken: newRefreshToken } = response.data;
+
+        // authUtils.changeToken(accessToken);
+        // authUtils.changeRefreshToken(newRefreshToken);
+
+        // axiosInstance.defaults.headers.common['Authorization'] =
+        //   `Bearer ${accessToken}`;
+
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        // Handle refresh token errors by clearing stored tokens and redirecting to the login page.
+        console.error('Failed to refresh token', refreshError);
+
+        window.alert(
+          'Your authentication credentials are expired, you will be redirected to the login page...',
+        );
+
+        // Clear tokens will trigger EntryPointProvider
+        authUtils.clearTokens();
+
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error); // For all other errors, return the error as is.
+  },
+);
---
+++ src/shared/cookie.ts
@@ -0,0 +1,41 @@
+import { AUTH_COOKIE_ID, AUTH_REFRESH_COOKIE_ID } from './config';
+import Cookies from 'universal-cookie';
+
+const commonCookieOptions = {
+  path: '/', // Valid for all URLs
+};
+
+/**
+ * Universal Cookies - Cookie helper that haves onChange listener (so helpful)!
+ */
+const cookies = new Cookies();
+
+/**
+ * Authentication Utility - Cookie Storage manipulation object
+ */
+export const authUtils = {
+  // Get Access Token
+  getToken: (): string => cookies.get(AUTH_COOKIE_ID),
+  changeToken: (token: string, expiresAt: Date) => {
+    cookies.set(AUTH_COOKIE_ID, token, {
+      ...commonCookieOptions,
+      expires: expiresAt,
+    });
+  },
+
+  // Get Refresh Token
+  getRefreshToken: (): string => cookies.get(AUTH_REFRESH_COOKIE_ID),
+  changeRefreshToken: (refreshToken: string, expiresAt: Date) => {
+    cookies.set(AUTH_REFRESH_COOKIE_ID, refreshToken, {
+      ...commonCookieOptions,
+      expires: expiresAt,
+    });
+  },
+
+  // Clear Access Token and Refresh Token
+  clearTokens: () => {
+    cookies.remove(AUTH_COOKIE_ID, { maxAge: 0, path: '/' });
+
+    cookies.remove(AUTH_REFRESH_COOKIE_ID, { maxAge: 0, path: '/' });
+  },
+};
