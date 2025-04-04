502cd52d420cf8c9f5ffc577d539581188ff8fce
hoangcao21 <hoangcao21@github.com>
2025-04-04 16:45:24 +0700

    chore: valid auth redirect user to current pathname

--- src/components/entry-point.tsx
+++ src/components/entry-point.tsx
@@ -1,9 +1,9 @@
 import { EntryPointContextProvider } from '../shared/contexts/entry-point';
 import { Auth, useAuth } from '../shared/hooks/useAuth';
-import { PATH_PAGE_AUTH, PATH_PAGE_HOME } from '../shared/routes';
+import { PATH_PAGE_AUTH } from '../shared/routes';
 import { Backdrop, CircularProgress } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet, useLocation, useNavigate } from 'react-router';
 
 /**
  * The entry point of the application
@@ -15,6 +15,8 @@ export const EntryPoint: FC = () => {
 
   const [loading, setLoading] = useState(false);
 
+  const location = useLocation();
+
   useEffect(() => {
     if (!auth.isValid) {
       console.warn('❌ No authentication credentials is present');
@@ -23,7 +25,7 @@ export const EntryPoint: FC = () => {
     } else {
       console.log('✅ Authentication credentials is present');
 
-      navigate(`/${PATH_PAGE_HOME}`);
+      navigate(`${location.pathname}`);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth.isValid]);
